fa90da3e9e1ebe57e268c577f3c9053d3553baef
pratik1243 <thakurpratik226@gmail.com>
2025-01-26 01:45:37 +0530

    data dashboard changes 18

--- src/components/LayoutComponents/NavBar.jsx
+++ src/components/LayoutComponents/NavBar.jsx
@@ -1,9 +1,38 @@
 import React from "react";
+import listIcon from "../../assets/images/list-icon-btn.png";
+import speakerIcon from "../../assets/images/speaker-icon.png";
+import profileNavIcon from "../../assets/images/nav-profile-icon.png";
+import notificationIcon from "../../assets/images/notification-icon.png";
+import searchIcon from "../../assets/images/search-icon.svg";
 
 const NavBar = () => {
   return (
     <div className="navbar-section">
       <span className="nav-head-txt">TECHYON</span>
+
+      <div>
+        <ul className="nav-menu-list-sec">
+          <li>
+            <img src={searchIcon} />
+          </li>
+          <li>
+            <img src={listIcon} />
+          </li>
+          <li>
+            <img src={speakerIcon} />
+          </li>
+          <li>
+            <button className="notification-btn">
+              <img src={notificationIcon} />
+
+              <span className="badge">+99</span>
+            </button>
+          </li>
+          <li>
+            <img src={profileNavIcon} className="nav-profile-btn" />
+          </li>
+        </ul>
+      </div>
     </div>
   );
 };
--- src/components/LayoutComponents/SideBar.jsx
+++ src/components/LayoutComponents/SideBar.jsx
@@ -1,9 +1,72 @@
-import React from 'react'
+import React from "react";
+import menuBtnIcon from "../../assets/images/menu-btn-icon.png";
+import sideBarIcon1 from "../../assets/images/side-bar-icon1.png";
+import sideBarIcon2 from "../../assets/images/side-bar-icon2.png";
+import sideBarIcon3 from "../../assets/images/side-bar-icon3.png";
+import sideBarIcon4 from "../../assets/images/side-bar-icon4.png";
+import sideBarIcon5 from "../../assets/images/side-bar-icon5.png";
+import sideBarIcon6 from "../../assets/images/side-bar-icon6.png";
+import sideBarIcon7 from "../../assets/images/side-bar-icon7.png";
+import sideBarIcon8 from "../../assets/images/side-bar-icon8.png";
+import sideBarIcon9 from "../../assets/images/side-bar-icon9.png";
+import sideBarIcon10 from "../../assets/images/side-bar-icon10.png";
+import sideBarIcon11 from "../../assets/images/side-bar-icon11.png";
+import sideBarIcon12 from "../../assets/images/side-bar-icon12.png";
 
 const SideBar = () => {
-  return (
-    <div className='sidebar-section'></div>
-  )
-}
+  const sideBarData = [
+    {
+      icon: menuBtnIcon,
+    },
+    {
+      icon: sideBarIcon1,
+    },
+    {
+      icon: sideBarIcon2,
+    },
+    {
+      icon: sideBarIcon3,
+    },
+    {
+      icon: sideBarIcon4,
+    },
+    {
+      icon: sideBarIcon5,
+    },
+    {
+      icon: sideBarIcon6,
+    },
+    {
+      icon: sideBarIcon7,
+    },
+    {
+      icon: sideBarIcon8,
+    },
+    {
+      icon: sideBarIcon9,
+    },
+    {
+      icon: sideBarIcon10,
+    },
+    {
+      icon: sideBarIcon11,
+    },
+    {
+      icon: sideBarIcon12,
+    },
+  ];
 
-export default SideBar
+  return (
+    <div className="sidebar-section">
+      {sideBarData.map((ele, index) => {
+        return (
+          <div key={index} className='side-menu-item'>
+            <img src={ele.icon} />
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default SideBar;
